b5695d3401daf50b423758087843c5bd48077d64
楓 若山 <waka0318.den@gmail.com>
2024-01-09 10:39:32 +0900

    自己紹介を追加

--- Ex30_15_wakayama/Program.cs
+++ Ex30_15_wakayama/Program.cs
@@ -6,7 +6,6 @@
         {
             Person person = new Person(new DateTime(2005,1,9), 80, "デジタル太郎",75 ,0.5f, 160f, 50f);
             person.Introduction(person);
-            Console.WriteLine("Hello World!こんにちは！さようなら");
         }
     }
 }
